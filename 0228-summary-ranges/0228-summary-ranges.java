42ad78143f263e1310f84d425f6ba5391265bb46
Abhishek Kumar Singh <149613026+abhisheksingh212@users.noreply.github.com>
2025-09-23 10:11:02 +0530

    Time: 5 ms (83.68%), Space: 41.9 MB (38.08%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res=new ArrayList<>();
+        int n=nums.length;
+
+        for(int i=0;i<n;i++)
+        { int start=nums[i];
+           
+           while(i+1<n && nums[i]+1 == nums[i+1])
+           {
+            i++;
+           }
+           if(start==nums[i])
+           {
+            res.add(String.valueOf(start));
+           }else{
+            res.add(start+"->"+nums[i]);
+           }
+        }
+        return res;
+    }
+}
